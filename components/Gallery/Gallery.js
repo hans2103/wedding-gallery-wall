0c27484f0de3299c019a2bc0d048d6bd5c17f4ad
Hans Kuijpers <hans2103@gmail.com>
2020-02-09 00:57:01 +0100

    add static html list items

--- components/Gallery/Gallery.js
+++ components/Gallery/Gallery.js
@@ -1,7 +1,66 @@
 export default function Gallery() {
 	return (
-		<ul>
-			<li>Gallery</li>
+		<ul className="gallery__grid">
+			<li className="gallery__item">
+				<figure className="gallery__figure">
+					<img src="https://images.unsplash.com/photo-1544568100-847a948585b9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+						 alt="A dog gazing at the sky and at a promising future. Or a nearby squirrel."/>
+				</figure>
+			</li>
+			<li className="gallery__item horizontal">
+				<figure className="gallery__figure">
+					<img src="https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+						 alt="A bulldog with a lot of moxie"/>
+				</figure>
+			</li>
+			<li className="gallery__item">
+				<figure className="gallery__figure">
+					<img src="https://images.unsplash.com/photo-1510771463146-e89e6e86560e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+						 alt="A daring dog carrying a flower between its romantic fangs"/>
+				</figure>
+			</li>
+			<li className="gallery__item">
+				<figure className="gallery__figure">
+					<img src="https://images.unsplash.com/photo-1507146426996-ef05306b995a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+						 alt="A cuddly young pup with a sassy red collar"/>
+				</figure>
+			</li>
+			<li className="gallery__item square">
+				<figure className="gallery__figure">
+					<img src="https://images.unsplash.com/photo-1530281700549-e82e7bf110d6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+						 alt="An extremely active dog jumping between the ocean's waves"/>
+				</figure>
+			</li>
+			<li className="gallery__item">
+				<figure className="gallery__figure">
+					<img src="https://images.unsplash.com/photo-1548199973-03cce0bbc87b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+						 alt="Two dogs cheerfully hopping along a beaten path. At least one of them is."/>
+				</figure>
+			</li>
+			<li className="gallery__item vertical">
+				<figure className="gallery__figure">
+					<img src="https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+						 alt="A dog diligently sitting and waiting for someone special, with a welcoming flower"/>
+				</figure>
+			</li>
+			<li className="gallery__item">
+				<figure className="gallery__figure">
+					<img src="https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+						 alt="A carefree dog licking its nose in a rather blurry picture"/>
+				</figure>
+			</li>
+			<li className="gallery__item">
+				<figure className="gallery__figure">
+					<img src="https://images.unsplash.com/photo-1535930891776-0c2dfb7fda1a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+						 alt="A smart, glass-wearing dog resting on an open book"/>
+				</figure>
+			</li>
+			<li className="gallery__item">
+				<figure className="gallery__figure">
+					<img src="https://images.unsplash.com/photo-1504595403659-9088ce801e29?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+						 alt="Two curious dogs looking above with great expectations"/>
+				</figure>
+			</li>
 		</ul>
 	)
 }
