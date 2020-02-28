90f1bb28674d65d805cc579268b4a44c38210b7f
Hans Kuijpers <hans2103@gmail.com>
2020-02-28 10:08:54 +0100

    Style warning

--- components/Warning/Warning.js
+++ components/Warning/Warning.js
@@ -1,9 +1,20 @@
-import css from './Warning.scss'
+import styled from 'styled-components';
 
 const Warning = () => (
-	<div className={css.gallery__message}>
+	<WarningContainer>
 		Sorry, your browser does not support CSS Grid. 😅
-	</div>
-)
+	</WarningContainer>
+);
+
+const WarningContainer = styled.div`
+  border: 1px solid #d2d0d0;
+  padding: 2em;
+  font-size: 1.7vw;
+  box-shadow: -2px 2px 10px 0px rgba(#444, 0.4);
+
+  @supports (display: grid) {
+    display: none;
+  }
+`;
 
 export default Warning;
